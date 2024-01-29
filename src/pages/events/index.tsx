6c2d115ace1aa6f6ba72f7b1c6c845b9fccb5fb0
NemesisX1 <juniormedehou@gmail.com>
2024-01-29 17:49:30 +0100

    (chore) seo

--- src/pages/events/index.tsx
+++ src/pages/events/index.tsx
@@ -12,6 +12,7 @@ import Six from '../../../public/biography/6.jpg';
 import Seven from '../../../public/biography/7.jpg';
 import Eight from '../../../public/biography/8.jpg';
 import Nine from '../../../public/biography/9.jpg';
+import { NextSeo } from 'next-seo';
 
 export default function Index() {
   // Animation Variants
@@ -74,106 +75,109 @@ export default function Index() {
     },
   ];
   return (
-    <motion.div
-      initial="initial"
-      animate="animate"
-      exit={{ opacity: 0 }}
-      className="font-cinzel"
-    >
-      <Header />
+    <>
+      <NextSeo />
+      <motion.div
+        initial="initial"
+        animate="animate"
+        exit={{ opacity: 0 }}
+        className="font-cinzel"
+      >
+        <Header />
 
-      {/* Hero Section */}
-      <section className="relative h-[50vh]">
-        <div className="container mx-auto h-full">
-          <img
-            src={IanTradi.src}
-            alt="Ian Mahinmi"
-            className="w-full h-full object-cover object-center" // Adjust image to cover the new height
-          />
+        {/* Hero Section */}
+        <section className="relative h-[50vh]">
+          <div className="container mx-auto h-full">
+            <img
+              src={IanTradi.src}
+              alt="Ian Mahinmi"
+              className="w-full h-full object-cover object-center" // Adjust image to cover the new height
+            />
 
-          <div
-            className="absolute top-0 left-0 w-full h-full flex items-center justify-center px-4"
-            style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}
-          >
-            <div className="text-center text-white">
-              <h1 className="text-4xl md:text-6xl font-bold">TSE</h1>
-              <p className="text-xl md:text-2xl font-light mt-4">
-                Ian Mahinmi (born November 5, 1986) is a French former
-                professional basketball player...
-              </p>
+            <div
+              className="absolute top-0 left-0 w-full h-full flex items-center justify-center px-4"
+              style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}
+            >
+              <div className="text-center text-white">
+                <h1 className="text-4xl md:text-6xl font-bold">TSE</h1>
+                <p className="text-xl md:text-2xl font-light mt-4">
+                  Ian Mahinmi (born November 5, 1986) is a French former
+                  professional basketball player...
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
-      <section className="i-am-foundation-section">
-        <div className="container mx-auto py-12 md:py-24">
-          <motion.div variants={fadeInUp}>
-            <h2 className="text-3xl font-bold">
-              I AM Foundation: Empowering Youth
-            </h2>
-            <p className="mt-4">
-              <a
-                href="https://iamfoundation.wpenginepowered.com/i-am/"
-                target="_blank"
-                className="gradient-link hover:underline font-semibold text-xl"
-              >
-                Ian Mahinmi I AM Foundation
-              </a>{' '}
-              is dedicated to{' '}
-              <strong>nurturing strong, vibrant communities</strong>. Our focus
-              is on fostering{' '}
-              <strong>service, leadership, and mentorship</strong> both locally
-              and globally. Our vision is to impact the lives of countless
-              children and adults, instilling in them a{' '}
-              <strong>healthy self-image</strong>, the ability to give and
-              receive love, and the drive to contribute positively to one
-              another and our planet. This ripple effect is our path to creating
-              a <strong>brighter future for all</strong>.
-              <br />
-              <br />
-              At the heart of our mission are core values that include{' '}
-              <strong>
-                Advocacy, Education, Integrity, Honesty, Compassion, Trust,
-                Contribution, Love, Leadership, Joy, Spirituality, Teaching,
-                Mastery, Creativity, Harmony, and Play
-              </strong>
-              . These values guide us in every action, every day, as we strive
-              to embody them within our community and beyond.
-            </p>
-          </motion.div>
-        </div>
-      </section>
+        </section>
+        <section className="i-am-foundation-section">
+          <div className="container mx-auto py-12 md:py-24">
+            <motion.div variants={fadeInUp}>
+              <h2 className="text-3xl font-bold">
+                I AM Foundation: Empowering Youth
+              </h2>
+              <p className="mt-4">
+                <a
+                  href="https://iamfoundation.wpenginepowered.com/i-am/"
+                  target="_blank"
+                  className="gradient-link hover:underline font-semibold text-xl"
+                >
+                  Ian Mahinmi I AM Foundation
+                </a>{' '}
+                is dedicated to{' '}
+                <strong>nurturing strong, vibrant communities</strong>. Our
+                focus is on fostering{' '}
+                <strong>service, leadership, and mentorship</strong> both
+                locally and globally. Our vision is to impact the lives of
+                countless children and adults, instilling in them a{' '}
+                <strong>healthy self-image</strong>, the ability to give and
+                receive love, and the drive to contribute positively to one
+                another and our planet. This ripple effect is our path to
+                creating a <strong>brighter future for all</strong>.
+                <br />
+                <br />
+                At the heart of our mission are core values that include{' '}
+                <strong>
+                  Advocacy, Education, Integrity, Honesty, Compassion, Trust,
+                  Contribution, Love, Leadership, Joy, Spirituality, Teaching,
+                  Mastery, Creativity, Harmony, and Play
+                </strong>
+                . These values guide us in every action, every day, as we strive
+                to embody them within our community and beyond.
+              </p>
+            </motion.div>
+          </div>
+        </section>
 
-      <section className="gallery-section">
-        <div className="container mx-auto py-12 md:py-24">
-          <motion.div variants={fadeIn}>
-            <h2 className="text-3xl font-bold text-center">Gallery</h2>
-            <p className="text-center mt-4 mb-8">
-              Explore Ian&apos;s journey through a curated collection of moments
-              from his career and foundation events.
-            </p>
-            <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
-              {imageData.map((image, index) => (
-                <div key={index} className="relative overflow-hidden">
-                  <img
-                    src={image.img.src}
-                    alt={`Gallery Image ${index + 1}`}
-                    className="w-full h-full object-cover object-center transition-transform duration-300 hover:scale-105"
-                    style={{ aspectRatio: '1 / 1' }}
-                  />
-                  <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 hover:opacity-100 flex justify-center items-center transition-opacity duration-300">
-                    <p className="text-white text-center">
-                      {image.description}
-                    </p>
+        <section className="gallery-section">
+          <div className="container mx-auto py-12 md:py-24">
+            <motion.div variants={fadeIn}>
+              <h2 className="text-3xl font-bold text-center">Gallery</h2>
+              <p className="text-center mt-4 mb-8">
+                Explore Ian&apos;s journey through a curated collection of
+                moments from his career and foundation events.
+              </p>
+              <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+                {imageData.map((image, index) => (
+                  <div key={index} className="relative overflow-hidden">
+                    <img
+                      src={image.img.src}
+                      alt={`Gallery Image ${index + 1}`}
+                      className="w-full h-full object-cover object-center transition-transform duration-300 hover:scale-105"
+                      style={{ aspectRatio: '1 / 1' }}
+                    />
+                    <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 hover:opacity-100 flex justify-center items-center transition-opacity duration-300">
+                      <p className="text-white text-center">
+                        {image.description}
+                      </p>
+                    </div>
                   </div>
-                </div>
-              ))}
-            </div>
-          </motion.div>
-        </div>
-      </section>
+                ))}
+              </div>
+            </motion.div>
+          </div>
+        </section>
 
-      <Footer />
-    </motion.div>
+        <Footer />
+      </motion.div>
+    </>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,7 @@ import Header from '@/components/Header';
 import Services from '@/components/Services';
 import Link from 'next/link';
 import ArticleSection from '@/components/Articles';
+import { NextSeo } from 'next-seo';
 function useConditionalMouse(ref: any) {
   return useMouse(ref, {
     enterDelay: 100,
@@ -171,171 +172,174 @@ export default function Index() {
   }, []);
 
   return (
-    <motion.div className="flex flex-col h-screen relative" ref={ref}>
-      <motion.div
-        // @ts-ignore
-        variants={variants}
-        className="circle"
-        animate={cursorVariant}
-        transition={spring}
-      >
-        <span className="cursorText font-clash ">{cursorText}</span>
-      </motion.div>
-
-      <Header />
-      {isModalOpen && (
+    <>
+      <NextSeo />
+      <motion.div className="flex flex-col h-screen relative" ref={ref}>
         <motion.div
-          className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
+          // @ts-ignore
+          variants={variants}
+          className="circle"
+          animate={cursorVariant}
+          transition={spring}
         >
-          <motion.video className=" mtz-vlc-dkbcc" autoPlay controls={true}>
+          <span className="cursorText font-clash ">{cursorText}</span>
+        </motion.div>
+
+        <Header />
+        {isModalOpen && (
+          <motion.div
+            className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <motion.video className=" mtz-vlc-dkbcc" autoPlay controls={true}>
+              <source src="hero.mp4" type="video/mp4" />
+            </motion.video>
+            <button
+              className="absolute top-4 right-4 text-white"
+              onClick={closeModal}
+            >
+              Close
+            </button>
+          </motion.div>
+        )}
+
+        <section
+          className="relative flex items-center min-h-screen bg-cover bg-center font-clash "
+          onMouseEnter={() => mouseEnter('play', 'play')}
+          onMouseLeave={() => mouseLeave('', 'default')}
+          onClick={openModal}
+        >
+          <motion.video
+            className="absolute inset-0 w-full h-full object-cover -z-10 bg-neutral2 mtz-vlc-dkbcc"
+            autoPlay
+            loop
+            muted
+            preload="meta"
+            style={
+              largeScreen > 1024
+                ? {
+                    scale,
+                    translateY,
+                    translateX,
+                    transition: 'all 1s ease-in-out',
+                    transformOrigin: 'right top',
+                  }
+                : {}
+            }
+          >
             <source src="hero.mp4" type="video/mp4" />
           </motion.video>
-          <button
-            className="absolute top-4 right-4 text-white"
-            onClick={closeModal}
-          >
-            Close
-          </button>
-        </motion.div>
-      )}
-
-      <section
-        className="relative flex items-center min-h-screen bg-cover bg-center font-clash "
-        onMouseEnter={() => mouseEnter('play', 'play')}
-        onMouseLeave={() => mouseLeave('', 'default')}
-        onClick={openModal}
-      >
-        <motion.video
-          className="absolute inset-0 w-full h-full object-cover -z-10 bg-neutral2 mtz-vlc-dkbcc"
-          autoPlay
-          loop
-          muted
-          preload="meta"
-          style={
-            largeScreen > 1024
-              ? {
-                  scale,
-                  translateY,
-                  translateX,
-                  transition: 'all 1s ease-in-out',
-                  transformOrigin: 'right top',
-                }
-              : {}
-          }
-        >
-          <source src="hero.mp4" type="video/mp4" />
-        </motion.video>
-        <div className="mix-blend-difference text-white font-bold text-center w-full left-1/2 -translate-x-1/2 absolute uppercase overflow-hidden bottom-0 mb-12 font-clash">
-          <div className="overflow-hidden">
-            <motion.div
-              initial={{ y: 100 }}
-              animate={{ y: 0 }}
-              style={{ translateY, transition: 'all 1.2s ease-in-out' }}
-              transition={{ delay: 0.3, ease: easing, duration: 0.5 }}
-              className="text-2xl sm:text-3xl md:text-5xl xl:text-7xl"
-            >
-              Shaping Future
-            </motion.div>
-          </div>
-          <div className="overflow-hidden">
-            <motion.div
-              initial={{ y: 100 }}
-              animate={{ y: 0 }}
-              style={{ translateY, transition: 'all 1.6s ease-in-out' }}
-              transition={{ delay: 0.5, ease: easing, duration: 0.7 }}
-              className="text-3xl sm:text-4xl md:text-6xl xl:text-8xl"
-            >
-              Inspiring Progress
-            </motion.div>
-          </div>
-          <div className="overflow-hidden">
-            <motion.div
-              initial={{ y: 150 }}
-              animate={{ y: 0 }}
-              style={{ translateY, transition: 'all 1.8s ease-in-out' }}
-              transition={{ delay: 0.7, ease: easing, duration: 0.9 }}
-              className="text-4xl sm:text-5xl md:text-7xl xl:text-9xl"
-            >
-              Creating Impact
-            </motion.div>
-          </div>
-          <motion.h3
-            initial={{ y: 200 }}
-            animate={{ y: 0 }}
-            transition={{ delay: 1, ease: easing, duration: 2.2 }}
-            style={{ translateY, transition: 'all 2.2s ease-in-out' }}
-            className="text-lg font-light"
-          >
-            (Scroll to explore)
-          </motion.h3>
-        </div>
-        <div className="mix-blend-screen text-lg text-white font-normal absolute bottom-0 p-4 font-clash w-full">
-          <div className="overflow-hidden flex justify-between">
+          <div className="mix-blend-difference text-white font-bold text-center w-full left-1/2 -translate-x-1/2 absolute uppercase overflow-hidden bottom-0 mb-12 font-clash">
+            <div className="overflow-hidden">
+              <motion.div
+                initial={{ y: 100 }}
+                animate={{ y: 0 }}
+                style={{ translateY, transition: 'all 1.2s ease-in-out' }}
+                transition={{ delay: 0.3, ease: easing, duration: 0.5 }}
+                className="text-2xl sm:text-3xl md:text-5xl xl:text-7xl"
+              >
+                Shaping Future
+              </motion.div>
+            </div>
+            <div className="overflow-hidden">
+              <motion.div
+                initial={{ y: 100 }}
+                animate={{ y: 0 }}
+                style={{ translateY, transition: 'all 1.6s ease-in-out' }}
+                transition={{ delay: 0.5, ease: easing, duration: 0.7 }}
+                className="text-3xl sm:text-4xl md:text-6xl xl:text-8xl"
+              >
+                Inspiring Progress
+              </motion.div>
+            </div>
+            <div className="overflow-hidden">
+              <motion.div
+                initial={{ y: 150 }}
+                animate={{ y: 0 }}
+                style={{ translateY, transition: 'all 1.8s ease-in-out' }}
+                transition={{ delay: 0.7, ease: easing, duration: 0.9 }}
+                className="text-4xl sm:text-5xl md:text-7xl xl:text-9xl"
+              >
+                Creating Impact
+              </motion.div>
+            </div>
             <motion.h3
               initial={{ y: 200 }}
               animate={{ y: 0 }}
-              style={{ translateY, transition: 'all 2.2s ease-in-out' }}
               transition={{ delay: 1, ease: easing, duration: 2.2 }}
-              className="text-xs sm:text-lg font-light"
-            >
-              Elevate Sports In Africa
-            </motion.h3>
-            <motion.h3
-              initial={{ y: 200 }}
-              animate={{ y: 0 }}
               style={{ translateY, transition: 'all 2.2s ease-in-out' }}
-              transition={{ delay: 1, ease: easing, duration: 2.2 }}
-              className="text-xs sm:text-lg font-light"
+              className="text-lg font-light"
             >
-              Based in Benin 🇧🇯
+              (Scroll to explore)
             </motion.h3>
           </div>
-        </div>
-      </section>
-      <section className="px-6 md:px-16 py-10 md:py-26 font-clash my-12">
-        <div className="flex flex-col w-full md:w-3/4">
-          <div className="flex">
-            <Star className="text-orange-700" />
-            <h1 className="text-4xl text-primary font-bold uppercase pb-8">
-              Who we are
-            </h1>
+          <div className="mix-blend-screen text-lg text-white font-normal absolute bottom-0 p-4 font-clash w-full">
+            <div className="overflow-hidden flex justify-between">
+              <motion.h3
+                initial={{ y: 200 }}
+                animate={{ y: 0 }}
+                style={{ translateY, transition: 'all 2.2s ease-in-out' }}
+                transition={{ delay: 1, ease: easing, duration: 2.2 }}
+                className="text-xs sm:text-lg font-light"
+              >
+                Elevate Sports In Africa
+              </motion.h3>
+              <motion.h3
+                initial={{ y: 200 }}
+                animate={{ y: 0 }}
+                style={{ translateY, transition: 'all 2.2s ease-in-out' }}
+                transition={{ delay: 1, ease: easing, duration: 2.2 }}
+                className="text-xs sm:text-lg font-light"
+              >
+                Based in Benin 🇧🇯
+              </motion.h3>
+            </div>
           </div>
-          <div className="scroll-reveal">
-            <p className="text-3xl lg:text-4xl mr-12 w-full md:w-3/4">
-              <span className="here">
-                Welcome to TAILORING SPORTS INVESTMENTS (TSI), founded in August
-                2023 by{' '}
-                <span
-                  className="font-semibold cursor-pointer animate-pulse"
-                  onMouseEnter={() => mouseEnter('', 'image')}
-                  onMouseLeave={() => mouseLeave('', 'default')}
-                >
-                  <Link href="/biography">Ian Mahinmi</Link>
-                </span>{' '}
-                , a seasoned entrepreneur and athlete with over 20 years of
-                experience in NBA and the FIBA. Our mission at TSI is to foster
-                the growth of the sports industry in Benin and Africa,
-                emphasizing innovation and excellence. Join us in shaping the
-                future of African sports.
-              </span>
-            </p>
+        </section>
+        <section className="px-6 md:px-16 py-10 md:py-26 font-clash my-12">
+          <div className="flex flex-col w-full md:w-3/4">
+            <div className="flex">
+              <Star className="text-orange-700" />
+              <h1 className="text-4xl text-primary font-bold uppercase pb-8">
+                Who we are
+              </h1>
+            </div>
+            <div className="scroll-reveal">
+              <p className="text-3xl lg:text-4xl mr-12 w-full md:w-3/4">
+                <span className="here">
+                  Welcome to TAILORING SPORTS INVESTMENTS (TSI), founded in
+                  August 2023 by{' '}
+                  <span
+                    className="font-semibold cursor-pointer animate-pulse"
+                    onMouseEnter={() => mouseEnter('', 'image')}
+                    onMouseLeave={() => mouseLeave('', 'default')}
+                  >
+                    <Link href="/biography">Ian Mahinmi</Link>
+                  </span>{' '}
+                  , a seasoned entrepreneur and athlete with over 20 years of
+                  experience in NBA and the FIBA. Our mission at TSI is to
+                  foster the growth of the sports industry in Benin and Africa,
+                  emphasizing innovation and excellence. Join us in shaping the
+                  future of African sports.
+                </span>
+              </p>
+            </div>
           </div>
-        </div>
-      </section>
-      <Services />
-      <ArticleSection
-        onMouseEnter={() => mouseEnter('View', 'article')}
-        onMouseLeave={() => mouseLeave('', 'default')}
-      />
-      <MultiLayerParallax
-        mouseEnter={() => mouseEnter('Lets talk', 'talk')}
-        mouseLeave={() => mouseLeave('', 'default')}
-      />
+        </section>
+        <Services />
+        <ArticleSection
+          onMouseEnter={() => mouseEnter('View', 'article')}
+          onMouseLeave={() => mouseLeave('', 'default')}
+        />
+        <MultiLayerParallax
+          mouseEnter={() => mouseEnter('Lets talk', 'talk')}
+          mouseLeave={() => mouseLeave('', 'default')}
+        />
 
-      <Footer />
-    </motion.div>
+        <Footer />
+      </motion.div>
+    </>
   );
 }
